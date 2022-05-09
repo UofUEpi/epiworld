afd610bf8d2fc7f92ffc43485d503593d35bfefa
gvegayon <gvegayon@users.noreply.github.com>
2022-05-09 20:18:29 +0000

    deploy: 197d8cc0e8fb86c06ec1ac2df6316e89d4857218

--- search/functions_4.js
+++ search/functions_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['record_5fvariant_68',['record_variant',['../class_data_base.html#a0058539b69a0082728b36df5c7f71d9f',1,'DataBase::record_variant()'],['../class_model.html#aecf6782e468b78459a37187903845d12',1,'Model::record_variant(Virus&lt; TSeq &gt; *v)']]],
+  ['record_5fvariant_68',['record_variant',['../class_data_base.html#a0058539b69a0082728b36df5c7f71d9f',1,'DataBase']]],
   ['reset_69',['reset',['../class_model.html#a9591eb76c7cf1ed7e1510342ff602d37',1,'Model']]],
   ['reset_5fstatus_5fcodes_70',['reset_status_codes',['../class_model.html#aa5819bef86c4c7d5623a7525831fb73c',1,'Model']]],
   ['run_71',['run',['../class_model.html#a825c7fb2498c7693cbf1bfcd0a416ef4',1,'Model']]],
