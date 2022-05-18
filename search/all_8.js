4dd2ef5fc445d8c03a8d57f46cf9a4289fb72b3b
gvegayon <gvegayon@users.noreply.github.com>
2022-05-18 17:28:31 +0000

    deploy: 7110f9921f8bdc0ac1335e9eb5b407fc1c008315

--- search/all_8.js
+++ search/all_8.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['randgraph_24',['RandGraph',['../class_rand_graph.html',1,'']]],
-  ['record_5fvariant_25',['record_variant',['../class_data_base.html#a0058539b69a0082728b36df5c7f71d9f',1,'DataBase']]],
+  ['record_5fvariant_25',['record_variant',['../class_data_base.html#a72493d18795fa53f627bb665850cd79b',1,'DataBase']]],
   ['reset_26',['reset',['../class_model.html#a9591eb76c7cf1ed7e1510342ff602d37',1,'Model']]],
   ['run_27',['run',['../class_model.html#a825c7fb2498c7693cbf1bfcd0a416ef4',1,'Model']]],
   ['run_5fmultiple_28',['run_multiple',['../class_model.html#a19824502152690539d276715589683c9',1,'Model']]]
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['record_5fvariant_66',['record_variant',['../class_data_base.html#a0058539b69a0082728b36df5c7f71d9f',1,'DataBase']]],
+  ['record_5fvariant_66',['record_variant',['../class_data_base.html#a72493d18795fa53f627bb665850cd79b',1,'DataBase']]],
   ['reset_67',['reset',['../class_model.html#a9591eb76c7cf1ed7e1510342ff602d37',1,'Model']]],
   ['run_68',['run',['../class_model.html#a825c7fb2498c7693cbf1bfcd0a416ef4',1,'Model']]],
   ['run_5fmultiple_69',['run_multiple',['../class_model.html#a19824502152690539d276715589683c9',1,'Model']]]
